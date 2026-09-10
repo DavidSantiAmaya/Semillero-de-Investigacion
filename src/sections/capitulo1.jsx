603e3b94bd0df02e56eb20fbbad456cdca5986ca
DavidSantiAmaya <160082902+DavidSantiAmaya@users.noreply.github.com>
2026-09-10 16:28:30 -0500

    public

--- src/sections/capitulo1.jsx
+++ src/sections/capitulo1.jsx
@@ -114,7 +114,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsLugares/boyaca-background.webp")} 
+                src={getAssetPath("AssetsLugares/boyaca-background.webp")} 
               />
             </span>
             <span className="floating-button-label">Boyacá (1819)</span>
@@ -127,7 +127,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsLugares/pantano-de-vargas-background.webp")}
+                src={getAssetPath("AssetsLugares/pantano-de-vargas-background.webp")}
               />
             </span>
             <span className="floating-button-label">Pantano de Vargas</span>
@@ -140,7 +140,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsHistoria/campana-libertadora.webp")}
+                src={getAssetPath("AssetsHistoria/campana-libertadora.webp")}
               />
             </span>
             <span className="floating-button-label">campaña libertadora</span>
@@ -153,7 +153,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsHistoria/batalla-gameza.webp")}
+                src={getAssetPath("AssetsHistoria/batalla-gameza.webp")}
               />
             </span>
             <span className="floating-button-label">Batalla de Gameza y Topaga</span>
@@ -185,7 +185,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsPersonajes/bolivar/card1.webp")}
+                src={getAssetPath("AssetsPersonajes/bolivar/card1.webp")}
               />
             </span>
             <span className="floating-button-label">Simón Bolívar</span>
@@ -198,7 +198,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsPersonajes/santander/card1.webp")}
+                src={getAssetPath("AssetsPersonajes/santander/card1.webp")}
               />
             </span>
             <span className="floating-button-label">Francisco de Paula Santander</span>
@@ -211,7 +211,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsLugares/pisba-background.webp")}
+                src={getAssetPath("AssetsLugares/pisba-background.webp")}
               />
             </span>
             <span className="floating-button-label">Paramo de Pisba</span>
@@ -224,7 +224,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsLugares/paipa-background.webp")}
+                src={getAssetPath("AssetsLugares/paipa-background.webp")}
               />
             </span>
             <span className="floating-button-label">Paipa</span>
@@ -237,7 +237,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsLugares/chicamocha-background.webp")}
+                src={getAssetPath("AssetsLugares/chicamocha-background.webp")}
               />
             </span>
             <span className="floating-button-label">Chicamocha</span>
@@ -252,7 +252,7 @@ const irAHistoria3 = () => {
           >
             <span className="floating-button-icon">
               <img
-                src={getAssetPath("public/AssetsHistoria/paramo-pisba.webp")}
+                src={getAssetPath("AssetsHistoria/paramo-pisba.webp")}
                 alt=""
               />
             </span>
